77eb09b4540e2952cd3e7e5a637f8b37e6138f06
djay-S <dhananjaysinghkushwaha2@gmail.com>
2025-07-26 20:17:02 +0530

    code changes for paged result of `getAllProducts`

--- catalog-service/src/main/java/org/demo/catalog_service/domain/ProductService.java
+++ catalog-service/src/main/java/org/demo/catalog_service/domain/ProductService.java
@@ -1,22 +1,39 @@
 package org.demo.catalog_service.domain;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.demo.catalog_service.ApplicationProperties;
+import org.demo.catalog_service.domain.records.PagedResult;
 import org.demo.catalog_service.domain.records.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @Transactional
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final ApplicationProperties properties;
 
-    public List<Product> getProducts(int pageNo) {
-        return productRepository.findAll()
-                .stream().map(ProductMapper::toProduct)
-                .toList();
+    public PagedResult<Product> getProducts(int pageNo) {
+        pageNo = Math.max(0, pageNo - 1);
+
+        Sort sort = Sort.by("name").ascending();
+        Pageable pageable = PageRequest.of(pageNo, properties.pageSize(), sort);
+        Page<Product> productsPage = productRepository.findAll(pageable).map(ProductMapper::toProduct);
+
+        return new PagedResult<>(
+                productsPage.getContent(),
+                productsPage.getTotalElements(),
+                productsPage.getNumber() + 1,
+                productsPage.getTotalPages(),
+                productsPage.isFirst(),
+                productsPage.isLast(),
+                productsPage.hasNext(),
+                productsPage.hasPrevious());
     }
 }
--- catalog-service/src/main/java/org/demo/catalog_service/web/controller/ProductController.java
+++ catalog-service/src/main/java/org/demo/catalog_service/web/controller/ProductController.java
@@ -2,22 +2,22 @@ package org.demo.catalog_service.web.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.demo.catalog_service.domain.ProductService;
+import org.demo.catalog_service.domain.records.PagedResult;
 import org.demo.catalog_service.domain.records.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
-public class ProductController {
+class ProductController {
 
     private final ProductService productService;
 
     @GetMapping
-    List<Product> getProducts(){
-        return productService.getProducts(0);
+    PagedResult<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
+        return productService.getProducts(pageNo);
     }
 }
