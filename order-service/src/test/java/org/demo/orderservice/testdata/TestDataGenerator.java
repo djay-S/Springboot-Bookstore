30f19302a6cb05c8139a13a00c4c1853d939b898
djay-S <dhananjaysinghkushwaha2@gmail.com>
2025-08-10 21:23:22 +0530

    order service - added `OrderControllerUnitTest`, `TestDataGenerator`, dependency of instancio to generate random test data

---
+++ order-service/src/test/java/org/demo/orderservice/testdata/TestDataGenerator.java
@@ -0,0 +1,57 @@
+package org.demo.orderservice.testdata;
+
+import org.demo.orderservice.domain.model.records.Address;
+import org.demo.orderservice.domain.model.records.CreateOrderRequest;
+import org.demo.orderservice.domain.model.records.Customer;
+import org.demo.orderservice.domain.model.records.OrderItem;
+import org.instancio.Instancio;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
+
+import static org.instancio.Select.field;
+
+/**
+ * A utility class for generating various test data related to orders.
+ * It uses the Instancio library to create instances of {@link CreateOrderRequest}
+ * with different configurations, including valid and invalid data for testing purposes.
+ */
+public class TestDataGenerator {
+    static final List<String> VALID_COUNTRIES = List.of("India", "Nepal");
+    static final Set<OrderItem> VALID_ORDER_ITEMS = Set.of(new OrderItem("P100", "Product 1", new BigDecimal("69.420"), 1));
+    static final Set<OrderItem> INVALID_ORDER_ITEMS = Set.of(new OrderItem("QWER", "Product 2", new BigDecimal("69.420"), 1));
+
+    public static CreateOrderRequest createValidOrderRequest() {
+        return Instancio.of(CreateOrderRequest.class)
+                .generate(field(Customer::email), generators -> generators.text().pattern("#a#a#a#a#a#a@mail.com"))
+                .set(field(CreateOrderRequest::items), VALID_ORDER_ITEMS)
+                .generate(field(Address::country), generators -> generators.oneOf(VALID_COUNTRIES))
+                .create();
+    }
+
+    public static CreateOrderRequest createOrderRequestWithInvalidCustomer() {
+        return Instancio.of(CreateOrderRequest.class)
+                .generate(field(Customer::email), gen -> gen.text().pattern("#c#c#c#c#d#d@mail.com"))
+                .set(field(Customer::phone), "")
+                .generate(field(Address::country), gen -> gen.oneOf(VALID_COUNTRIES))
+                .set(field(CreateOrderRequest::items), VALID_ORDER_ITEMS)
+                .create();
+    }
+
+    public static CreateOrderRequest createOrderRequestWithInvalidDeliveryAddress() {
+        return Instancio.of(CreateOrderRequest.class)
+                .generate(field(Customer::email), gen -> gen.text().pattern("#c#c#c#c#d#d@mail.com"))
+                .set(field(Address::country), "")
+                .set(field(CreateOrderRequest::items), VALID_ORDER_ITEMS)
+                .create();
+    }
+
+    public static CreateOrderRequest createOrderRequestWithNoItems() {
+        return Instancio.of(CreateOrderRequest.class)
+                .generate(field(Customer::email), gen -> gen.text().pattern("#c#c#c#c#d#d@mail.com"))
+                .generate(field(Address::country), gen -> gen.oneOf(VALID_COUNTRIES))
+                .set(field(CreateOrderRequest::items), Set.of())
+                .create();
+    }
+}
---
+++ order-service/src/test/java/org/demo/orderservice/web/controllers/OrderControllerUnitTest.java
@@ -0,0 +1,76 @@
+package org.demo.orderservice.web.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.demo.orderservice.domain.OrderService;
+import org.demo.orderservice.domain.SecurityService;
+import org.demo.orderservice.domain.model.records.CreateOrderRequest;
+import org.demo.orderservice.testdata.TestDataGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Named.named;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Unit tests for the {@link OrderController} using MockMvc.
+ * This class focuses on testing the controller's behavior in isolation
+ * by mocking its dependencies, such as {@link OrderService} and {@link SecurityService}.
+ */
+@WebMvcTest(OrderController.class)
+public class OrderControllerUnitTest {
+    @MockitoBean
+    private OrderService orderService;
+
+    @MockitoBean
+    private SecurityService securityService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        given(securityService.getLoginUserName()).willReturn("djay");
+    }
+
+    static Stream<Arguments> createOrderRequestProvider() {
+        return Stream.of(
+                Arguments.arguments(named("Order with Invalid Customer", TestDataGenerator.createOrderRequestWithInvalidCustomer())),
+                Arguments.arguments(named("Order with Invalid Delivery Address", TestDataGenerator.createOrderRequestWithInvalidDeliveryAddress())),
+                Arguments.arguments(named("Order with No Items", TestDataGenerator.createOrderRequestWithNoItems()))
+        );
+    }
+
+    /**
+     * This test is a **Parameterized Test**, meaning it will be executed multiple times,
+     * once for each set of arguments provided by a source. The source for this test
+     * is specified by the {@link MethodSource} annotation.
+     * The **{@link MethodSource}** annotation points to the static method
+     * {@code createOrderRequestProvider}, which provides the different test cases.
+     * This allows for a clean separation of test data from the test logic.
+     *
+     * @param request The invalid {@link CreateOrderRequest} payload to be tested.
+     * @throws Exception if an error occurs during the mock MVC request.
+     */
+    @ParameterizedTest(name = "[{index}]-{0}")
+    @MethodSource("createOrderRequestProvider")
+    void shouldReturnBadRequestWhenOrderPayloadIsInvalid(CreateOrderRequest request) throws Exception {
+        mockMvc.perform(post("/api/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(request))
+        ).andExpect(status().isBadRequest());
+    }
+}
