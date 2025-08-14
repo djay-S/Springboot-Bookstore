d709fe55b62d02f4aadfd583964b68a0f135b663
djay-S <dhananjaysinghkushwaha2@gmail.com>
2025-08-14 20:58:19 +0530

    catalog service - code change for resilience demo

--- catalog-service/src/main/java/org/demo/catalog_service/web/controller/ProductController.java
+++ catalog-service/src/main/java/org/demo/catalog_service/web/controller/ProductController.java
@@ -22,9 +22,18 @@ class ProductController {
 
     @GetMapping("/{code}")
     ResponseEntity<Product> getProductByCode(@PathVariable String code) {
+        sleep();
         return productService
                 .getProductByCode(code)
                 .map(ResponseEntity::ok)
                 .orElseThrow(() -> ProductNotFoundException.forCode(code));
+    }
+
+    private void sleep () {
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
