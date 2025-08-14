d28fada3317a805ac9a5bc9c03cf9aeacd069952
djay-S <dhananjaysinghkushwaha2@gmail.com>
2025-08-14 20:59:35 +0530

    order service - handled Retry resilience through @Retry and `application.properties` attributes

--- order-service/src/main/java/org/demo/orderservice/clients/catalog/ProductServiceClient.java
+++ order-service/src/main/java/org/demo/orderservice/clients/catalog/ProductServiceClient.java
@@ -1,6 +1,8 @@
 package org.demo.orderservice.clients.catalog;
 
 import java.util.Optional;
+
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -12,18 +14,30 @@ import org.springframework.web.client.RestClient;
 public class ProductServiceClient {
     private final RestClient restClient;
 
+/**
+ * <p>
+ * `@Retry` is resilience 4j annotation used to retry the request if any exception is thrown by the method.
+ * This means the exception should not be consumed completely by the getProductByCode, hence the try - catch was removed
+ *
+ * By default @Retry will retry for 3 times and would wait for 500ms for each retry.
+ * </p>
+ * name: This is used to give a backend name to this retry.
+ *            This is used when configuring this in application.properties<br>
+ * fallbackMethod: This defines the name of the method to be called after all the retries have failed
+* */
+    @Retry(name = "catalog-service", fallbackMethod = "getProductByCodeFallback")
     public Optional<Product> getProductByCode(String code) {
         log.info("Fetching product for code: {}", code);
-        try {
             var product = restClient
                     .get()
                     .uri("/api/products/{code}", code)
                     .retrieve()
                     .body(Product.class);
             return Optional.ofNullable(product);
-        } catch (Exception e) {
-            log.error("Error in fetching product for code: {}", code, e);
-            return Optional.empty();
-        }
+    }
+
+    Optional<Product> getProductByCodeFallback(String code, Throwable throwable) {
+        log.warn("ProductServiceClient.getProductByCodeFallback: code: {}", code);
+        return Optional.empty();
     }
 }
