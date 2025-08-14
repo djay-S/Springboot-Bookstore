a2f9ffe4b512eb651c67f8643f868d8b308dd38d
djay-S <dhananjaysinghkushwaha2@gmail.com>
2025-08-14 21:27:12 +0530

    order service - handled Circuit breaker resilience through @CircuitBreaker and `application.properties` attributes

--- order-service/src/main/java/org/demo/orderservice/clients/catalog/ProductServiceClient.java
+++ order-service/src/main/java/org/demo/orderservice/clients/catalog/ProductServiceClient.java
@@ -2,6 +2,7 @@ package org.demo.orderservice.clients.catalog;
 
 import java.util.Optional;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +25,18 @@ public class ProductServiceClient {
  * name: This is used to give a backend name to this retry.
  *            This is used when configuring this in application.properties<br>
  * fallbackMethod: This defines the name of the method to be called after all the retries have failed
+ *
+ * <p>
+ * `@CircuitBreaker` is resilience 4j annotation to add circuit breaker to the endpoint.
+ * Types and properties are defined in application.properties using the backend name.
+ * Refer to actuators: {@link http://localhost:8082/actuator/circuitbreakers}, {@link http://localhost:8082/actuator/circuitbreakerevents}
+ * for circuit breaker status.
+ * If a fallbackMethod is added to circuit breaker, once the breaker opens it would return from the fallback method.
+ * Retry assumes that endpoint executed successfully and would not retry the request.
+ * This happens since CircuitBreaker has higher precedence than Retry. This precedence order is configurable.
+ * </p>
 * */
+    @CircuitBreaker(name = "catalog-service")
     @Retry(name = "catalog-service", fallbackMethod = "getProductByCodeFallback")
     public Optional<Product> getProductByCode(String code) {
         log.info("Fetching product for code: {}", code);
