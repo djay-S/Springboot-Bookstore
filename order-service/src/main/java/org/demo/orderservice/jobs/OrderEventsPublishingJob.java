b488330b7b8d0929c62b4e753a8c92cfc2606f05
djay-S <dhananjaysinghkushwaha2@gmail.com>
2025-08-28 21:27:36 +0530

    order service - added ShedLock

--- order-service/src/main/java/org/demo/orderservice/jobs/OrderEventsPublishingJob.java
+++ order-service/src/main/java/org/demo/orderservice/jobs/OrderEventsPublishingJob.java
@@ -1,12 +1,13 @@
 package org.demo.orderservice.jobs;
 
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.demo.orderservice.domain.OrderEventService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.time.Instant;
 
 @Slf4j
 @Component
@@ -15,7 +16,9 @@ class OrderEventsPublishingJob {
     private final OrderEventService orderEventService;
 
     @Scheduled(cron = "${orders.publish-order-events-job-cron}")
+    @SchedulerLock(name = "publishOrderEvents")
     public void publishOrderEvents() {
+        LockAssert.assertLocked();
         log.info("Publishing Order Events at {}", Instant.now());
         orderEventService.publishOrderEvents();
     }
--- order-service/src/main/java/org/demo/orderservice/jobs/OrderProcessingJob.java
+++ order-service/src/main/java/org/demo/orderservice/jobs/OrderProcessingJob.java
@@ -1,12 +1,13 @@
 package org.demo.orderservice.jobs;
 
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.demo.orderservice.domain.OrderService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.time.Instant;
 
 @Slf4j
 @Component
@@ -15,7 +16,9 @@ public class OrderProcessingJob {
     private final OrderService orderService;
 
     @Scheduled(cron = "${orders.new-orders-job-cron}")
+    @SchedulerLock(name = "processNewOrders")
     public void processNewOrders() {
+        LockAssert.assertLocked();
         log.info("Processing new orders at: {}", Instant.now());
         orderService.processNewOrders();
     }
