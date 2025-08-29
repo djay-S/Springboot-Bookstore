ab30e199acacf76a1e868d822a01dfbebf0218a3
djay-S <dhananjaysinghkushwaha2@gmail.com>
2025-08-29 20:15:27 +0530

    order service - comment to trigger build

--- order-service/src/main/java/org/demo/orderservice/config/SchedulerConfig.java
+++ order-service/src/main/java/org/demo/orderservice/config/SchedulerConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-
 @Configuration
 public class SchedulerConfig {
 
