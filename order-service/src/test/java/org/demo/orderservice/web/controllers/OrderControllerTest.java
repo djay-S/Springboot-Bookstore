61ac20887f268156f3bd6ddcc3b41234e97b3861
djay-S <dhananjaysinghkushwaha2@gmail.com>
2025-08-31 12:47:27 +0530

    order service - added test cases for getOrder and getOrders APIs, added `test-orders.sql` and @Sql annotation to maintain consistent db state for test executions

--- order-service/src/test/java/org/demo/orderservice/web/controllers/OrderControllerTest.java
+++ order-service/src/test/java/org/demo/orderservice/web/controllers/OrderControllerTest.java
@@ -1,17 +1,29 @@
 package org.demo.orderservice.web.controllers;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.*;
 
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
 import java.math.BigDecimal;
+import java.util.List;
+
 import org.demo.orderservice.AbstractIntegrationTest;
+import org.demo.orderservice.domain.model.records.OrderSummary;
 import org.demo.orderservice.testdata.TestDataGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
 
+/*
+* For every test method this sql script is executed.
+* By default, if the file is not specified, it checks for classpath
+* */
+@Sql("/test-orders.sql")
 class OrderControllerTest extends AbstractIntegrationTest {
 
     @Nested
@@ -63,6 +75,38 @@ class OrderControllerTest extends AbstractIntegrationTest {
                     .post("/api/orders")
                     .then()
                     .statusCode(HttpStatus.BAD_REQUEST.value());
+        }
+    }
+
+    @Nested
+    class GetOrdersTest {
+        @Test
+        void shouldGetOrdersSuccessfully() {
+            List<OrderSummary> orderSummaries = given().when()
+                    .get("/api/orders")
+                    .then()
+                    .statusCode(HttpStatus.OK.value())
+                    .extract()
+                    .body()
+                    .as(new TypeRef<>() {
+                    });
+
+            assertThat(orderSummaries).hasSize(2);
+        }
+    }
+
+    @Nested
+    class GetOrderByOrderNumberTests {
+        String orderNumber = "order-123";
+
+        @Test
+        void shouldGetOrderSuccessfully() {
+            given().when()
+                    .get("/api/orders/{orderNumber}", orderNumber)
+                    .then()
+                    .statusCode(HttpStatus.OK.value())
+                    .body("orderNumber", is(orderNumber))
+                    .body("items.size()", is(2));
         }
     }
 }
