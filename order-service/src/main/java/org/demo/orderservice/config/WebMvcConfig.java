13096a0d9b6361165cb98659073e7e5e4908ed53
djay-S <dhananjaysinghkushwaha2@gmail.com>
2025-09-06 20:37:29 +0530

    api-gateway, catalog, order service - code refactor

--- order-service/src/main/java/org/demo/orderservice/config/WebMvcConfig.java
+++ order-service/src/main/java/org/demo/orderservice/config/WebMvcConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//This cors configuration is implemented to handle redirection from Swagger documentation.
+// This cors configuration is implemented to handle redirection from Swagger documentation.
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
