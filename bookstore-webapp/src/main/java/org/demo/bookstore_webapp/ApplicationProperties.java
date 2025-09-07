a790793c39eb86b7981293f452904a455bce47b9
djay-S <dhananjaysinghkushwaha2@gmail.com>
2025-09-07 21:00:40 +0530

    bookstore-webapp - comment to trigger build

--- bookstore-webapp/src/main/java/org/demo/bookstore_webapp/ApplicationProperties.java
+++ bookstore-webapp/src/main/java/org/demo/bookstore_webapp/ApplicationProperties.java
@@ -2,5 +2,6 @@ package org.demo.bookstore_webapp;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+// Extra comment to trigger build
 @ConfigurationProperties(prefix = "bookstore")
 public record ApplicationProperties(String apiGatewayUrl) {}
