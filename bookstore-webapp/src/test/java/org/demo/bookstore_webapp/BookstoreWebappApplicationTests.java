261ee16c5b5064251f6a5ea44d55a1445db62e2c
djay-S <dhananjaysinghkushwaha2@gmail.com>
2025-09-07 17:47:53 +0530

    bookstore-webapp - added new module

---
+++ bookstore-webapp/src/test/java/org/demo/bookstore_webapp/BookstoreWebappApplicationTests.java
@@ -0,0 +1,11 @@
+package org.demo.bookstore_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreWebappApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
